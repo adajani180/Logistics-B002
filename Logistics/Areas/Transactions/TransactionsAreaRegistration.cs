697255ba30731839db8e954367b59131bdce3bb1
Ricky White <rickywhite@outlook.com>
2018-12-12 15:54:08 -0500

    Copied over Transactions module to new repo

---
+++ Logistics/Areas/Transactions/TransactionsAreaRegistration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Logistics.Areas.Transactions
+{
+    public class TransactionsAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Transactions";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Transactions_default",
+                "Transactions/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
