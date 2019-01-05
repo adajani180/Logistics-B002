b7717446f79a9fa6bb4cace7fa0df5d8a10a7369
Ricky White <rickywhite@outlook.com>
2019-01-05 15:37:00 -0500

    Added Transactions Context and Entity Class

---
+++ Logistics/Entities/Transaction/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Logistics.Entities.Transaction
+{
+    [Table("TransactionTracking")]
+    public class Transaction
+    {
+        public int Id { get; set; } // int - not null
+
+        [ForeignKey("")]
+        public int ItemNum { get; set; } // int - not null
+
+        [ForeignKey("")]
+        public int? EquipNum { get; set; } // int
+        public string? TransactionType { get; set; } // string
+        public int? Qty { get; set; } // int
+        public string? EmpId { get; set; } // string
+
+        [ForeignKey("")]
+        public int? NewEmpId { get; set; } // bigint
+        public string? Vin { get; set; } // string
+        public string? UnitId { get; set; } // string
+        public string? ModifiedBy { get; set; } // string
+        public DateTime? ModifiedDate { get; set; } // datetime
+
+
+    }
+}
---
+++ Logistics/Models/TransactionContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Entity;
+using Logistics.Entities;
+
+namespace Logistics.Models
+{
+    public class TransactionContext : DbContext
+    {
+        public TransactionContext()
+            : base("name=SatisDB")
+        {
+        }
+
+        public virtual DbSet<Transaction> Transactions { get; set; }
+
+        //public virtual DbSet<ID> Id{ get; set; } // int - not null
+        //public virtual DbSet<ItemNum> ItemNum { get; set; } // int - not null
+        //public virtual DbSet<EquipNum> EquipNum { get; set; } // int
+        //public virtual DbSet<TransactionType> TransactionType { get; set; } // string
+        //public virtual DbSet<Qty> Quantity { get; set; } // int
+        //public virtual DbSet<EmpId> EmployeeId { get; set; } // string
+        //public virtual DbSet<NewEmpId> NewEmployeeId { get; set; } // bigint
+        //public virtual DbSet<Vin> Vin { get; set; } // string
+        //public virtual DbSet<UnitId> UnitId { get; set; } // string
+        //public virtual DbSet<ModifiedBy> ModifiedBy { get; set; } // string
+        //public virtual DbSet<ModifiedDate> ModifiedDate { get; set; } // datetime
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+
+    }
+}
