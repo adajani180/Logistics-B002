9874d92c57cdc46c4c491ff1d0b4e3c8fe9d5c4f
Ricky White <rickywhite@outlook.com>
2019-01-10 10:44:06 -0500

    Added Find(), Get(), GetAll() and Save() logic to TransactionsRepository.cs

--- Logistics/Entities/Transaction/Transaction.cs
+++ Logistics/Entities/Transaction/Transaction.cs
@@ -8,22 +8,17 @@ namespace Logistics.Entities.Transaction
     {
         public int Id { get; set; } // int - not null
 
-        [ForeignKey("")]
-        public int ItemNum { get; set; } // int - not null
+        public int ItemNum { get; set; } // int - not null - FK
 
-        [ForeignKey("")]
-        public int EquipNum { get; set; } // int
+        public int EquipNum { get; set; } // int - FK
         public string TransactionType { get; set; } // string
         public int Qty { get; set; } // int
         public string EmpId { get; set; } // string
 
-        [ForeignKey("")]
-        public int NewEmpId { get; set; } // bigint
+        public int NewEmpId { get; set; } // bigint - FK
         public string Vin { get; set; } // string
         public string UnitId { get; set; } // string
         public string ModifiedBy { get; set; } // string
         public DateTime ModifiedDate { get; set; } // datetime
-
-
     }
 }
--- Logistics/Repositories/Transaction/TransactionRepository.cs
+++ Logistics/Repositories/Transaction/TransactionRepository.cs
@@ -12,6 +12,8 @@ namespace Logistics.Repositories
 {
     public class TransactionRepository : IRepository<Transaction>
     {
+        TransactionContext context = new TransactionContext();
+
         public void Delete(Transaction entity)
         {
             throw new NotImplementedException();
@@ -22,24 +24,61 @@ namespace Logistics.Repositories
             throw new NotImplementedException();
         }
 
-        public IEnumerable<Transaction> Find(Expression<Func<Transaction, bool>> predicate)
-        {
-            throw new NotImplementedException();
-        }
 
+        // Find a Transaction
+        public IEnumerable<Transaction> Find(Expression<Func<Transaction, bool>> predicate) => this.context.Transactions.AsNoTracking().Where(predicate);
+
+
+        // Get a Single Transaction
         public Transaction Get(object id)
         {
-            throw new NotImplementedException();
+            int tranId = int.Parse(id.ToString());
+
+            var tran = this.Find(t => t.Id == tranId).SingleOrDefault();
+
+            return tran;
         }
 
-        public IEnumerable<Transaction> GetAll()
-        {
-            throw new NotImplementedException();
-        }
+        // Get All Transactions
+        public IEnumerable<Transaction> GetAll() => this.context.Transactions;
 
+
+        // Create or Save a Transaction
         public void Save(Transaction entity)
         {
-            throw new NotImplementedException();
+            Transaction tran = this.Get(entity.Id);
+            if (tran == null)
+            {
+                entity.ItemNum = tran.ItemNum;
+                entity.EmpId = tran.EmpId;
+                entity.EquipNum = tran.EquipNum;
+                entity.ModifiedBy = null;
+                entity.ModifiedDate = DateTime.Now;
+                entity.NewEmpId = tran.NewEmpId;
+                entity.Qty = tran.Qty;
+                entity.TransactionType = tran.TransactionType;
+                entity.UnitId = tran.UnitId;
+                entity.Vin = tran.Vin;
+
+                this.context.Transactions.Add(entity);
+            }
+            else
+            {
+                entity.ItemNum = tran.ItemNum;
+                entity.EmpId = tran.EmpId;
+                entity.EquipNum = tran.EquipNum;
+                entity.ModifiedBy = tran.ModifiedBy;
+                entity.ModifiedDate = DateTime.Now;
+                entity.NewEmpId = tran.NewEmpId;
+                entity.Qty = tran.Qty;
+                entity.TransactionType = tran.TransactionType;
+                entity.UnitId = tran.UnitId;
+                entity.Vin = tran.Vin;
+
+                this.context.Entry(tran).State = EntityState.Modified;
+            }
+            
+            this.context.SaveChanges();
         }
     }
 }
